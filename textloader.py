c2d030c8221a1682cc6d103becab01da96485a29
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-06 23:32:11 -0700

    text loader run successfully

--- textloader.py
+++ textloader.py
@@ -2,4 +2,5 @@ from langchain_community.document_loaders import TextLoader
 
 loader = TextLoader("sample_data.txt")
 documents = loader.load()
-print(documents)
+print(documents[0])
+print(len(documents))
