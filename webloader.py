50fb4fbd2ff9bc9a42c7ced368722bed5431a7d2
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-07 01:27:17 -0700

    webloader added

---
+++ webloader.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import WebBaseLoader
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate,PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough,RunnableLambda,RunnableBranch
+
+load_dotenv()
+
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Retrieval-augmented_generation")
+documents = loader.load()
+
+
+model=ChatGroq(
+    model_name="llama-3.3-70b-versatile",
+    temperature=0
+)
+
+prompt=PromptTemplate(
+    input_variables=["context","question"],
+    template="""
+    Use the following context to answer the question.
+    Context: {context}
+    Question: {question}
+    """
+)
+
+parser=StrOutputParser()
+
+chain=prompt | model | parser
+
+print(chain.invoke({"context":documents[0].page_content,"question":"What is RAG?"}))
