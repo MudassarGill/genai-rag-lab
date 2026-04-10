4e4337bb0e75a75136a38736afcc6a74fbd21197
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-10 02:06:30 -0700

    three doc added

--- chormadb.py
+++ chormadb.py
@@ -2,20 +2,43 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
-
+from langchain_core.documents import Document
 load_dotenv()
 
-loader = PyPDFLoader("M Mudassar Hussain.pdf")
-documents = loader.load()
+doc1=Document(
+    page_content="Babar azam is a very good batsman.He is very good at cover drive. and Fakhar Zaman is a very good batsman.",
+    metadata={
+        "source": "doc1",
+        "author": "M Mudassar Hussain"
+    }
+)
 
-embeddings = HuggingFaceEmbeddings(
+doc2=Document(
+    page_content="Shoaib Akhtar is a very good bowler.He is very fast bowler He is very populer in the world.",
+    metadata={
+        "source": "doc2",
+        "author": "M Mudassar Hussain"
+    }
+)
+
+doc3=Document(
+    page_content="Wasim Akram is a very good bowler.He is very fast bowler He is very populer in the world.",
+    metadata={
+        "source": "doc3",
+        "author": "M Mudassar Hussain"
+    }
+)
+
+docs=[doc1,doc2,doc3]
+
+embeddings=HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
     model_kwargs={"device": "cpu"},
     encode_kwargs={"normalize_embeddings": False},
 )
 
-vectorstore = Chroma.from_documents(
-    documents=documents,
+vectorstore=Chroma.from_documents(
+    documents=docs,
     embedding=embeddings,
     persist_directory="./.chroma_db"
 )
