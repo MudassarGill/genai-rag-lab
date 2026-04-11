c01861ccf250f916ebfc727bf65f85cb2f33e208
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-11 06:53:53 -0700

    wikipediaretriever added

--- retrivers.py
+++ retrivers.py
@@ -5,6 +5,6 @@ retrivers=WikipediaRetriever(top_k_results=2)
 query='What is AI and tell me its types?'
 docs=retrivers.invoke(query)
 
-
-for i , doc e(docs):
-    print()
+for i , doc in  enumerate(docs):
+    print(f'\n----Resutl {i+1}----')
+    print(f'content --> \n {doc.page_content}..')
