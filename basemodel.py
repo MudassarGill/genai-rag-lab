1729696f3bb3e28367a31f4edd34b0f8c4a6bb4d
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-13 01:03:36 -0700

    basemodel tools

---
+++ basemodel.py
@@ -0,0 +1,6 @@
+from langchain.tools import BaseTool
+from typing import type
+
+
+
+class MultipleTool(BaseTool):
--- tools.py
+++ tools.py
@@ -5,26 +5,26 @@ from langchain.tools import DuckDuckGoSearchRun
 
 search=DuckDuckGoSearchRun()
 print(search.invoke('Latest news in the world'))
-# @tool
+@tool
 
-# def add(a:int,b:int)->int:
-#     """Add two numbers"""
-#     return a+b
+def add(a:int,b:int)->int:
+    """Add two numbers"""
+    return a+b
 
-# @tool
-# def sub(a:int,b:int)->int:
-#     """Subtract two numbers"""
-#     return a-b
+@tool
+def sub(a:int,b:int)->int:
+    """Subtract two numbers"""
+    return a-b
 
-# @tool
-# def mul(a:int,b:int)->int:
-#     """Multiply two numbers"""
-#     return a*b
+@tool
+def mul(a:int,b:int)->int:
+    """Multiply two numbers"""
+    return a*b
 
-# @tool
-# def div(a:int,b:int)->int:
-#     """Divide two numbers"""
-#     return a/b
+@tool
+def div(a:int,b:int)->int:
+    """Divide two numbers"""
+    return a/b
 
 
 
