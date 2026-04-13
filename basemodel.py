908d46efd80600c6ec0898254403820739906d82
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-13 01:04:33 -0700

    basemodel tool suing pydantic

--- basemodel.py
+++ basemodel.py
@@ -4,3 +4,11 @@ from typing import type
 
 
 class MultipleTool(BaseTool):
+    a: int = Field(description="First number")
+    b: int = Field(description="Second number")
+    name: str = "Multiple"
+    description: str = "Multiply two numbers"
+    def _run(self,a:int,b:int)->int:
+        return a*b
+    def _arun(self,a:int,b:int)->int:
+        return a*b
