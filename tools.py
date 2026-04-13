d192e942b106b42e2ba05a0747173c3029d5b623
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-13 00:54:46 -0700

    duck duck search tool added

--- tools.py
+++ tools.py
@@ -1,21 +1,25 @@
 from langchain.tools import tool
+from langchain.tools import DuckDuckGoSearchRun
 
-@tool
-def add(a:int,b:int)->int:
-    """Add two numbers"""
-    return a+b
+# @tool
+# def add(a:int,b:int)->int:
+#     """Add two numbers"""
+#     return a+b
 
-@tool
-def sub(a:int,b:int)->int:
-    """Subtract two numbers"""
-    return a-b
+# @tool
+# def sub(a:int,b:int)->int:
+#     """Subtract two numbers"""
+#     return a-b
 
-@tool
-def mul(a:int,b:int)->int:
-    """Multiply two numbers"""
-    return a*b
+# @tool
+# def mul(a:int,b:int)->int:
+#     """Multiply two numbers"""
+#     return a*b
 
-@tool
-def div(a:int,b:int)->int:
-    """Divide two numbers"""
-    return a/b
+# @tool
+# def div(a:int,b:int)->int:
+#     """Divide two numbers"""
+#     return a/b
+
+
+
