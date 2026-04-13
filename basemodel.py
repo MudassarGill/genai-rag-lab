cf2bc4499baec32383c2f90e8073025b715d67a7
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-13 01:07:06 -0700

    multiplytools

--- basemodel.py
+++ basemodel.py
@@ -1,14 +1,23 @@
+from git import Tree
+from torch.optim.optimizer import required
 from langchain.tools import BaseTool
 from typing import type
+from pydantic import Field
 
 
 
-class MultipleTool(BaseTool):
-    a: int = Field(description="First number")
-    b: int = Field(description="Second number")
-    name: str = "Multiple"
+class Multipleinput(BaseTool):
+    a: int = Field(required=True,description="First number")
+    b: int = Field(required=True,description="Second number")
+
+
+class Multiplytools(BaseTool):
+    name: str = "Multiply"
     description: str = "Multiply two numbers"
+    args_schema: type[BaseTool] = Multipleinput
     def _run(self,a:int,b:int)->int:
         return a*b
     def _arun(self,a:int,b:int)->int:
         return a*b
+
+    
